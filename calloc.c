d167e65e7d10ee9e7328542a80d24670a0e54d2b
alokd9919911-source <alokd9919911@gmail.com>
2026-09-17 10:49:52 +0530

    Dynamic  calloc  in c

---
+++ calloc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *ptr;
+    scanf("%d", &n);
+    ptr = (int *) calloc(n, sizeof(int));
+
+    ptr[0] = 4;
+    ptr[1] = 7;
+    printf("%d", ptr[0]);
+
+    return 0;
+}
