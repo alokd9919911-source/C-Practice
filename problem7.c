871eccd33a804215ed3522d92d5d8344ba96de7a
alokd9919911-source <alokd9919911@gmail.com>
2026-09-17 22:47:50 +0530

    using calloc and realloc print 5& 10 numbers

---
+++ problem7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n = 5;
+    int *ptr;
+    ptr = (int *)calloc(n, sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+
+        scanf("%d", &ptr[i]);
+    }
+    printf("The array of:");
+
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("%d\n", ptr[i]);
+    }
+    int n = 10;
+    ptr = (int *)realloc(ptr, 10 * sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+
+        scanf("%d", &ptr[i]);
+    }
+    printf("The array of:");
+    for (int i = 0; i < n; i++)
+    {
+
+        printf("%d\n", ptr[i]);
+    }
+    return 0;
+}
