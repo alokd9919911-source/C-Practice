0a33095932b5671a50a95f45f38f71364a3c2612
alokd9919911-source <alokd9919911@gmail.com>
2026-09-17 11:14:02 +0530

    Realloc in c

---
+++ Realloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int *ptr;
+    scanf("%d", &n);
+    ptr = (int *) calloc(n, sizeof(int));
+
+    ptr[0] = 4;
+    ptr[1] = 7;
+    printf("%d", ptr[0]);
+    ptr = (int*) realloc(ptr, 10* sizeof (int));
+    
+    return 0;
+}
