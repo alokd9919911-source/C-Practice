53f48fee73942a383b7dd79a75dd965bf8fd0778
alokd9919911-source <alokd9919911@gmail.com>
2026-09-24 20:31:25 +0530

    calloc in c

--- calloc.c
+++ calloc.c
@@ -5,11 +5,14 @@ int main()
 {
     int n;
     int *ptr;
+    
     scanf("%d", &n);
+
     ptr = (int *) calloc(n, sizeof(int));
 
     ptr[0] = 4;
     ptr[1] = 7;
+
     printf("%d", ptr[0]);
 
     return 0;
